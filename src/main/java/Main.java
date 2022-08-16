30892d8080e0dbaf525980d9273b384548284d96
Mimmey <motyolya@gmail.com>
2022-08-16 14:31:33 +0500

    Added some comments

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,16 @@ import Solution.Task0;
 
 import java.util.Scanner;
 
+/**
+ * Используем только для финальных решений (для закидывания в контест).
+ * Для локальных тестов используем тесты из пакета test.
+ */
+
 public class Main {
+    /**
+     * Для каждой задачи переписываем код main, чтоб
+     * передавать разные параметры из разных источников.
+     */
     public static void main(String[] args) {
         try (Scanner scanner = new Scanner(System.in)) {
             int a = scanner.nextInt();
